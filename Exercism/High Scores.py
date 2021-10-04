f95efe964b16bf19345bb8a49b2ebbb4f3b973cf
Lev Sirotkin-Shreter <schroterl@yandex.ru>
2021-10-04 12:12:07 +0300

    Create High Scores.py

---
+++ Exercism/High Scores.py
@@ -0,0 +1,13 @@
+# High Scores
+# https://exercism.org/tracks/python/exercises/high-scores
+
+def latest(scores):
+    return scores[-1]
+
+
+def personal_best(scores):
+    return max(scores)
+
+
+def personal_top_three(scores):
+    return sorted(scores, reverse=True)[:3]
